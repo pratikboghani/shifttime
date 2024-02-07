a021347e2ab624b00eed9619d5adaecfcc611eff
pratikboghani1 <ppatel5858@gmail.com>
2024-02-07 00:11:21 -0500

    readme

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const clrGeenOriginal = Color(0xff43a756);
+const clrGreenOriginalLight = Color(0xFFd9eddd);
+const clrGreenWhite10 = Color(0xFF56b067);
+const clrGreenWhite20 = Color(0xFF69b978);
+const clrGreenWhite30 = Color(0xFF7bc189);
+const clrGreenWhite40 = Color(0xFF8eca9a);
+const clrGreenWhite50 = Color(0xFFa1d3ab);
+const clrGreenWhite60 = Color(0xFFb4dcbb);
+const clrGreenWhite70 = Color(0xFFc7e5cc);
+const clrGreenWhite80 = Color(0xFFd9eddd);
+const clrGreenWhite90 = Color(0xFFecf6ee);
+const clrGreenDark10 = Color(0xFF3c964d);
+const clrGreenDark20 = Color(0xFF368645);
+const clrGreenDark30 = Color(0xFF2f753c);
+const clrGreenDark40 = Color(0xFF286434);
+const clrGreenDark50 = Color(0xFF22542b);
+const clrGreenDark60 = Color(0xFF1b4322);
+const clrGreenDark70 = Color(0xFF14321a);
+const clrGreenDark80 = Color(0xFF0d2111);
+const clrGreenDark90 = Color(0xFF071109);
+
+const clrWhite = Colors.white;
+const blueColor = Color(0xFF1B5A77);
+const greyColor = Colors.black;
+//Color(0xFF5E5F5E);
+const apiPrefix = 'http://103.155.115.118:8686/';
+
+const welcomeTextStyle = TextStyle(
+  fontSize: 23,
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontFamily: fontFamily,
+);
+
+const welcomeTextWidget = Text(
+  'Welcome,',
+  style: welcomeTextStyle,
+);
+
+const fontFamily = 'Rubik';
