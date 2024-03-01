1b63c6b7988a186dccf8487b952a2079686f0aa2
pratikboghani1 <ppatel5858@gmail.com>
2024-02-29 23:18:50 -0500

    Availability list user side

--- lib/components/availability_form.dart
+++ lib/components/availability_form.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shifttime/utilities/constants.dart';
 import 'package:shifttime/utilities/raised_button_widget.dart';
 import 'package:shifttime/utilities/time_picker.dart';
+
+import '../models/availabilityDetails.dart';
 
 class AvailabilityForm extends StatefulWidget {
   const AvailabilityForm({super.key});
@@ -26,26 +31,200 @@ class _AvailabilityFormState extends State<AvailabilityForm> {
   final TextEditingController fridayTController = TextEditingController();
   final TextEditingController saturdayFController = TextEditingController();
   final TextEditingController saturdayTController = TextEditingController();
+  List<dynamic> availabilityList = [];
+  String IsAvailable(String startTime, String endTime){
+    if(startTime.isEmpty || endTime.isEmpty){
+      return 'UNAVAILABLE';
+    }
+    return 'AVAILABLE';
+  }
+  Future<void> _saveAvailability() async {
+    final List<AvailabilityDetails> availabilityDetailsList = [
+      AvailabilityDetails(
+        dayOfWeek: 'SUNDAY',
+        startTime: sundayFController.text,
+        endTime: sundayTController.text,
+        availabilityType: IsAvailable(sundayFController.text,sundayTController.text),
+      ),
+      AvailabilityDetails(
+        dayOfWeek: 'MONDAY',
+        startTime: mondayTController.text,
+        endTime: mondayFController.text,
+        availabilityType: IsAvailable(mondayTController.text,mondayFController.text),
+      ),
+      AvailabilityDetails(
+        dayOfWeek: 'TUESDAY',
+        startTime: tuesdayFController.text,
+        endTime: tuesdayTController.text,
+        availabilityType: IsAvailable(tuesdayFController.text,tuesdayTController.text),
+      ),
+      AvailabilityDetails(
+        dayOfWeek: 'WEDNESDAY',
+        startTime: wednesdayFController.text,
+        endTime: wednesdayTController.text,
+        availabilityType: IsAvailable(wednesdayFController.text,wednesdayTController.text),
+      ),
+      AvailabilityDetails(
+        dayOfWeek: 'THURSDAY',
+        startTime: thursdayFController.text,
+        endTime: thursdayTController.text,
+        availabilityType: IsAvailable(thursdayFController.text,thursdayTController.text),
+      ),
+      AvailabilityDetails(
+        dayOfWeek: 'FRIDAY',
+        startTime: fridayFController.text,
+        endTime: fridayTController.text,
+        availabilityType: IsAvailable(fridayFController.text,fridayTController.text),
+      ),
+      AvailabilityDetails(
+        dayOfWeek: 'SATURDAY',
+        startTime: saturdayFController.text,
+        endTime: saturdayTController.text,
+        availabilityType: IsAvailable(saturdayFController.text,saturdayTController.text),
+      ),
+    ];
 
+    final Map<String, dynamic> requestBody = {
+      'userId': userId,
+      'availibilityDetails': availabilityDetailsList.map((detail) => detail.toJson()).toList(),
+      'notes': 'my note',
+    };
+    void _showSnackbar(String message,Color color) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          message,
+          style: const TextStyle(color: Colors.white),
+        ),
+
+        duration: const Duration(seconds: 2),
+        backgroundColor: color,
+      ));
+    }
+    final http.Response response = await http.post(
+      Uri.parse('$apiPrefix/availability/create/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $userToken',
+      },
+      body: jsonEncode(requestBody),
+    );
+print(requestBody);
+print(response.body);
+    if (response.statusCode == 200) {
+      // Successfully saved
+      _showSnackbar('Availability saved successfully', Colors.green);
+      Navigator.of(context).pop(); // Close the bottom sheet
+    } else {
+      // Handle error
+      _showSnackbar('Failed to save availability. Status code: ${response.statusCode}', Colors.green);
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    _fetchAvailability();
+  }
+
+  Future<void> _fetchAvailability() async {
+    try {
+      final http.Response response = await http.get(
+        Uri.parse('$apiPrefix/availability'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $userToken',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> responseBody = jsonDecode(response.body);
+        final List<dynamic> docs = responseBody['response']['docs'];
+        setState(() {
+          availabilityList = docs;
+        });
+      } else {
+        print('Failed to fetch availability. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching availability: $error');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: double.infinity,
-          ),
-          // Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: RaisedButtonWidget(
-          //     bgColor: clrGreenOriginal,
-          //     buttonText: 'Add Availability',
-          //     buttonTextColor: clrWhite,
-          //     onPressed: () => _showAddAvailabilityPopup(context),
-          //   ),
-          // ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+            ),
+        
+            Column(
+              children: availabilityList.map((availability) {
+                return Card(
+                  margin: const EdgeInsets.all(16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Table(
+                      columnWidths: const {
+                        0: FlexColumnWidth(2),
+                        1: FlexColumnWidth(1),
+                        2: FlexColumnWidth(1),
+                      },
+                      children: [
+                        TableRow(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Text(
+                                'Approved: ${availability['isApproved']}',
+                                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(), // Empty cell to align with the start_time column
+                            Container(), // Empty cell to align with the end_time column
+                          ],
+                        ),
+                        for (int i = 0; i < availability['availibilityDetails'].length; i++)
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text(
+                                  '${availability['availibilityDetails'][i]['day_of_week']}',
+                                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text('${availability['availibilityDetails'][i]['start_time']}'),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text('${availability['availibilityDetails'][i]['end_time']}'),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+        
+                // return ListTile(
+                //   title: Text('Approved: ${availability['isApproved']}'),
+                //   subtitle: Text(
+                //     '${availability['availibilityDetails'][0]['day_of_week']}: ${availability['availibilityDetails'][0]['start_time']} to ${availability['availibilityDetails'][0]['end_time']}',
+                //   ),
+                // );
+              }).toList(),
+            ),
+        
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: () => _showAddAvailabilityPopup(context),child: const Icon(Icons.add)),
     );
@@ -201,9 +380,7 @@ class _AvailabilityFormState extends State<AvailabilityForm> {
                   buttonTextColor: clrWhite,
                   bgColor: clrGreenOriginal,
                   onPressed: () {
-
-
-
+                    _saveAvailability();
                     Navigator.of(context).pop();
                   },
                 ),
@@ -215,4 +392,3 @@ class _AvailabilityFormState extends State<AvailabilityForm> {
     );
   }
 }
-
---
+++ lib/models/availabilityDetails.dart
@@ -0,0 +1,22 @@
+class AvailabilityDetails {
+  final String dayOfWeek;
+  final String startTime;
+  final String endTime;
+  final String availabilityType; // Assuming you have an availability type
+
+  AvailabilityDetails({
+    required this.dayOfWeek,
+    required this.startTime,
+    required this.endTime,
+    required this.availabilityType,
+  });
+
+  Map<String, String> toJson() {
+    return {
+      'day_of_week': dayOfWeek,
+      'start_time': startTime,
+      'end_time': endTime,
+      'availability_type': availabilityType,
+    };
+  }
+}
